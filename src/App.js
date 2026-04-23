e9b887e9a62b699fed93ad317294785115c42b78
Muhammad Shan Shoukat <muhammadshanshoukat004@gmail.com>
2026-04-24 00:35:11 +0500

    Bg Color To Image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
  
 
-  <div style={{backgroundImage: `url(${BgImage})`,}}className='border rounded-2xl w-[300px] h-[500px] bg-blue-400 flex flex-row justify-center mt-16 relative left-[600px]  bg-gradient-to-br from-[#6a11cb] via-[#8e2de2] to-[#ff4e9b]' >
+  <div style={{backgroundImage: `url(${BgImage})`,}}className='border rounded-2xl w-[300px] h-[500px] bg-blue-400 flex flex-row justify-center mt-16 relative left-[600px] ' >
 <WeatherApp  />
 
   </div> </>
